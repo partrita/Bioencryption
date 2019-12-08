2e0405b9e34b3ea161260b76c2819a4d79049e6a
deepthought <partrita@gmail.com>
2019-12-08 12:28:50 +0000

    initial commit

---
+++ bioencrytion.py
@@ -0,0 +1,18 @@
+# argparse
+
+# translation table
+
+table = {}
+
+# define functions
+def main():
+    pass
+
+def text_cleaning(text):
+    pass
+
+def translation():
+    pass
+
+if "__name__" == "__main__":
+    main()
